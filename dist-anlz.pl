9a066610ea4ce5713656ddcefeb05f9e56db13e4
Ricardo Signes <rjbs@cpan.org>
2015-04-20 14:11:26 +0200

    we're using postderef, so use it

--- dist-anlz.pl
+++ dist-anlz.pl
@@ -1,4 +1,4 @@
-use 5.12.0;
+use 5.20.0;
 use warnings;
 use experimental 'postderef';
 use List::Util 'sum0';
@@ -12,8 +12,8 @@ my $agg    = $ARGV[1] // 50;
 Analyze->aggregate_minorities($result, $agg);
 
 my $dz_results = $result->{'Dist::Zilla'};
-my $count   = @{ $dz_results->{distfiles} };
-my $authors = keys %{ $dz_results->{author} };
+my $count      = $dz_results->{distfiles}->@*;
+my $authors    = keys $dz_results->{author}->%*;
 
 printf "There are %s dists by %s unique authors using Dist::Zilla.\n\n",
   $count,
@@ -24,7 +24,7 @@ my $table = Text::Table->new('generator', \' | ', 'dists', \' | ', '%');
 my $total = sum0 map {; 0 + $_->{distfiles}->@* } values %$result;
 
 for my $key (
-  sort { @{ $result->{$b}{distfiles} } <=> @{ $result->{$a}{distfiles} } }
+  sort { $result->{$b}{distfiles}->@* <=> $result->{$a}{distfiles}->@* }
   keys %$result
 ) {
   my $count = $result->{$key}{distfiles}->@*;
