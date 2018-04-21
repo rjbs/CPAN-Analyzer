b1db6e957c6114ebfe6a37fe5563e9c4da97c6c2
Ricardo Signes <rjbs@cpan.org>
2018-04-21 16:37:35 +0200

    graph.pl: fewer warnings, less ugly

--- graph.pl
+++ graph.pl
@@ -1,13 +1,16 @@
 #!/usr/bin/env perl
-use strict;
+use v5.20.0;
 use warnings;
+use experimental 'postderef';
 
 use Data::Dumper;
 
 use Aggregate;
 use Chart::Clicker;
+use Chart::Clicker::Axis::DateTime;
 use Chart::Clicker::Data::Series;
 use Chart::Clicker::Data::DataSet;
+use Date::Parse;
 
 my $chart = Chart::Clicker->new; # build the chart
 
@@ -27,18 +30,17 @@ for my $file (glob('*.csv'), glob('*.sqlite')) {
   for my $tool (keys %$result) {
     $series{ $tool } ||= [];
     $series{ $tool }[ $#keys ] = @{ $result->{$tool}{distfiles} };
+
+    $_ //= 0 for $series{ $tool }->@*;
   }
 }
 
-my @totals;
+my @totals = (0) x @keys;;
 for my $i (0 .. $#keys) {
   for my $set (keys %series) {
-    $totals[ $i ] += $series{$set}[$i];
+    $totals[ $i ] += $series{$set}[$i] // 0;
   }
 }
-
-use Data::Dumper;
-warn Dumper(\@totals);
 
 for my $set (
   sort { $series{$b}[0] <=> $series{$a}[0] } keys %series
@@ -51,7 +53,7 @@ for my $set (
 
   my $series = Chart::Clicker::Data::Series->new(
     name   => $set || '(blank)',
-    keys   => [ @keys ],
+    keys   => [ map {; str2time($_) } @keys ],
     values => [ map { $series{$set}[$_] / $totals[$_] * 100 } (0..$#keys) ],
   );
 
@@ -69,6 +71,13 @@ for my $set (
 $chart->get_context('default')->range_axis->range(
   Chart::Clicker::Data::Range->new(lower => 0, upper => 50)
 );
+
+$chart->get_context('default')->domain_axis(
+  Chart::Clicker::Axis::DateTime->new(
+    tick_label_angle => '5',
+    orientation => 'horizontal',
+    position    => 'bottom',
+));
 
 # write the chart to a file
 $chart->write_output('chart.png');
